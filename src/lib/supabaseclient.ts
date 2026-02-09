53e9eb8f490d889e1d61ed6473c3e9301df0f34f
ginaqueenval <akhalvat560@gmail.com>
2026-02-09 16:18:43 +0000

    Fix supabase client import case for Vercel

---
+++ src/lib/supabaseclient.ts
@@ -0,0 +1,9 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+
+// برای Client Components
+export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey);
